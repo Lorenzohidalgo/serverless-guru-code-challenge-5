746bfae01e3a51f9cd16048cdef28fa281c6797f
Lorenzo Hidalgo <lorenzo.hidalgo@e-campus.uab.cat>
2022-11-07 16:23:43 +0100

    implemented update endpoint

--- functions/update_order.js
+++ functions/update_order.js
@@ -2,10 +2,19 @@
 
 const AWS = require("aws-sdk");
 
+const PossibleOrderStatus = ["OPEN", "INPROGRESS", "READY", "CLOSED"];
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, _, callback) => {
+  const ISODateTime = new Date().toISOString();
+  const data = JSON.parse(event.body);
+
   if (
+    !(
+      typeof data.status === "string" &&
+      PossibleOrderStatus.includes(data.status)
+    ) ||
     !(
       typeof event.pathParameters.id === "string" &&
       event.pathParameters.id.length !== 0
@@ -15,7 +24,7 @@ module.exports.handler = (event, _, callback) => {
     callback(null, {
       statusCode: 400,
       headers: { "Content-Type": "text/plain" },
-      body: "Validation Failed. Couldn't get the order item.",
+      body: "Validation Failed. Couldn't update the order item.",
     });
     return;
   }
@@ -25,25 +34,36 @@ module.exports.handler = (event, _, callback) => {
     Key: {
       id: event.pathParameters.id,
     },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+    ExpressionAttributeValues: {
+      ":status": data.status,
+      ":updatedAt": ISODateTime,
+    },
+    UpdateExpression:
+      "SET #status = :status, updatedAt = :updatedAt",
+    ReturnValues: "ALL_NEW",
   };
 
-  // fetch order from the database
-  dynamoDb.get(params, (error, result) => {
+  // update the order in the database
+  dynamoDb.update(params, (error, result) => {
     // handle potential errors
     if (error) {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { "Content-Type": "text/plain" },
-        body: "Couldn't fetch the order item.",
+        body: "Couldn't fetch the todo item.",
       });
       return;
     }
-
+    
+    console.log(result);
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify({ data: result.Item }),
+      body: JSON.stringify({data: result.Attributes}),
     };
     callback(null, response);
   });
